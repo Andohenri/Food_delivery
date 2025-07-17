93560d26d7c3d6c441d7432d9baba41b33a5c334
DjangoBona <andohenrirazafinatonadro@gmail.com>
2025-07-17 10:26:18 +0300

    Implementing home screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontLoaded, error] = useFonts({
+    "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "Quicksand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "Quicksand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "Quicksand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if(error) throw error;
+    if(fontLoaded) SplashScreen.hideAsync();
+  }, [fontLoaded, error])
+  
+  return <Stack screenOptions={{headerShown: false}} />;
 }
